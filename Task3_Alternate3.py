071d0db27ea136a7b62bd8703630cbd998171029
akishore <akishore@salesforce.com>
2015-09-22 16:03:17 +0530

    Task 3 Updated with comments

--- Task3_Alternate3.py
+++ Task3_Alternate3.py
@@ -2,13 +2,16 @@
 
 __author__ = "Amar"
 
+# Given List To Be Changed.
 days = ['yesterday', 'today', 'tomorrow', 'dayafter', 'amardeep']
 
-print(days)
+print(days) # Printing the initial list.
 
-for Index_val in days:
-    Index_num = days.index(Index_val)
-    day = Index_val[:Index_num+1].upper() + Index_val[Index_num+1:]
-    days[Index_num] = day
+for Index_val in days:                                              # Simple loop to save the object of list as string in variable.
+    Index_num = days.index(Index_val)                               # Extracting the Index number from the List using the object saved above.
+    day = Index_val[:Index_num+1].upper() + Index_val[Index_num+1:] #Concatinating, Modifying and Adding back the original string.
+    days[Index_num] = day                                           #Finally updating original List with new values
 
+# Printing the new list
+print(" ")
 print(days)
