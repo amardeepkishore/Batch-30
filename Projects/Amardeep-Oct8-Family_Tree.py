2f4388826c9d2863cbad4cc11397bd3689a8d4ff
akishore <akishore@salesforce.com>
2015-10-11 02:16:12 +0530

    Finally Fourth project Working - FAMILY TREE

--- Projects/Amardeep-Oct8-Family_Tree.py
+++ Projects/Amardeep-Oct8-Family_Tree.py
@@ -3,40 +3,92 @@
 __author__ = 'akishore'
 
 
-''' The family tree includes only the first names of female members of the family,
-    mothers and daughters, through many generations. '''
+'''
+The family tree includes only the first names of female members of the family,
+    mothers and daughters, through many generations.
+
+This function will take three* parameters:
+    1. A string representing the family tree
+    2. The name of the first person
+    3. The name of another person
+
+* Follow the instructions for more legibility.
+
+Here are several examples of different pairs of people and the result for the tree as above:
+    1. IN: Hazel & Gloria             OUT: Clare             (they have a direct common mother)
+    2. IN: Hazel & Clare              OUT: Clare             (one is the mother of the other)
+    3. IN: Hazel & Flora              OUT: Ann                (closest is their grandmother)
+    4. IN: Hazel & Betty              OUT: Ann                (the only ancestor in common is Ann)
+    5. IN: Hazel & Ann                OUT: Ann                (one is the grandmother of the other)
+    6. IN: Hazel & Hazel             OUT: Hazel              (they are the same person - just return that person)
+
+Note:- This Programme will give you more verbosity than the user cases shown above and a functionality to add new members too.
+'''
 
 # Importing Module
 import sys
 import re
-import pprint
 from os import system
-ftree = {'anna':['a1','a2'], 'lazy':['b1','b2'], 'a1':['x1','x2'], 'x1':['z1','z2']}
-#ftree = {}
-pp = pprint.PrettyPrinter(indent=4, width=80,depth=None)
+
+ftree = {"Ann": ["Betty", "Clare"], "Betty": ["Donna", "Elizabeth", "Flora"], "Clare": ["Gloria", "Hazel"]}
+# ftree = {}
+
+# Global Declaration for user input.
 first = ''
 second = ''
 
+# Below variable should be used only one time in programme.
+_mother1 = ''
+_mother2 = ''
 
+
+# System Exit function.
 def systm_exit():
     print("Exiting on user input...")
     sys.exit(0)
 
 
+# Finding Mother of Input Function.
+def find_mother():
+    try:
+        if True in [first in ftree.values()[i] for i in range(len(ftree.values()))]:
+            global _mother1
+            _mother1 = ftree.items()[[first in ftree.values()[i] for i in range(len(ftree.values()))].index(True)][0]
+            return _mother1
+        elif True in [second in ftree.values()[i] for i in range(len(ftree.values()))]:
+            global _mother2
+            _mother2 = ftree.items()[[second in ftree.values()[i] for i in range(len(ftree.values()))].index(True)][0]
+            return _mother2
+    except ValueError:
+        return "No Mother Found".upper()
+
+
+# Finding Grandmother of Mother function.
+def find_gmother():
+    try:
+        if first in ftree.get(find_mother()):
+            grandmother1 = ftree.items()[[_mother1 in ftree.values()[i] for i in range(len(ftree.values()))].index(True)][0]
+            return grandmother1
+        elif second in ftree.get(find_mother()):
+            grandmother2 = ftree.items()[[_mother2 in ftree.values()[i] for i in range(len(ftree.values()))].index(True)][0]
+            return grandmother2
+    except ValueError:
+        return "No One".upper()
+
+
+# List The Families Tree
 def mem_tree():
-    uchoice = raw_input("Do you want to display Tree in Unix style 'Y' or 'N': ")
-    if re.match('^yes$|^y$',uchoice,re.I):
         for i, j in ftree.iteritems():
             print(i)
-            print('.'*len(i) + "|__ {}".format(j[0]))
-            print('.'*len(i) + "|__ {}\n".format(j[1]))
-    elif re.match('^no$|^n$',uchoice,re.I):
-        pp.pprint(ftree)
-    else:
-        print("Provide a valid Input.\n")
+            for k in range(len(j)):
+                    print('.'*len(i) + "|__ {}".format(j[k]))
+            print("")
 
+
+# Add new members if not found in List.
 def mem_add():
-    keyorvalue = raw_input("If you want to add {} as a Mother type '1' and to add {} as a Daughter of other Mother\'s type '2'.: ".format(first.upper(),first.upper()))
+    keyorvalue = raw_input("If you want to add {} as a Mother type '1'\
+ and to add {} as a Daughter of other Mother\'s type '2'.: ".format(first.upper(), first.upper()))
     print("")
     if keyorvalue == '1':
         daughter_len = '0'
@@ -63,51 +115,123 @@ def mem_add():
         print("WRONG VALUE PROVIDED, TYPE EITHER '1' or '2\n")
 
 
+# Mem Add simplified.
+def mem_add_sure():
+    sures = raw_input("{} is not found, do you want to add it now. 'y' or 'n': ".format(second.upper()))
+    while True:
+        if re.match('^n$',sures,re.I):
+            return systm_exit()
+        elif re.match('^y$',sures,re.I):
+            return mem_add()
+        else:
+            return "Error: input not valid: Try again.\n"
+
+
+def case1():
+    try:
+        if [first in ftree.values()[i] for i in range(len(ftree.values()))].index(True) == [second in ftree.values()[i] for i in range(len(ftree.values()))].index(True):
+            xyz = "{} and {} are Sisters and their Mother and Grandmother are {} and {} respectively.\n".format(first.upper(), second.upper(), find_mother(), find_gmother())
+        return xyz
+    except:
+        pass
+
+
+def case2():
+    try:
+        if second in ftree.keys() and first in ftree.get(second):
+            return "{} is mother of {}\n".format(second.upper(),first.upper())
+        elif first in ftree.keys() and second in ftree.get(first):
+            return "{} is the mother of {}\n".format(first.upper(),second.upper())
+    except:
+        pass
+
+
+def case3():
+    try:
+        if (first not in ftree.keys() or second not in ftree.keys())\
+                and ([first in ftree.values()[i] for i in range(len(ftree.values()))][[first in ftree.values()[i] for i in range(len(ftree.values()))].index(True)]\
+                or [second in ftree.values()[i] for i in range(len(ftree.values()))][[second in ftree.values()[i] for i in range(len(ftree.values()))].index(True)]) is True:
+            return "{} and {}'s closest is their Grandmother {}.\n ".format(first, second, find_gmother())
+    except:
+        pass
+
+
+def case4():
+    try:
+        if first not in ftree.keys() and\
+                ([second in ftree.values()[i] for i in range(len(ftree.values()))][[second in ftree.values()[i] for i in range(len(ftree.values()))].index(True)] is True\
+                and second in ftree.keys()) or second not in ftree.keys() and ([first in ftree.values()[i] for i in range(len(ftree.values()))][[first in ftree.values()[i]\
+                for i in range(len(ftree.values()))].index(True)] is True and first in ftree.keys()):
+            return "The Only Ancestor Common between {} and {} is {}.\n ".format(first, second, find_gmother())
+    except:
+        pass
+
+
+def case5():
+    try:
+        if (first not in ftree.keys() and second in ftree.keys()) and second not in [j for j in [i for i in ftree.values()] if second not in j][0]:
+            return "{} is the Grandmother of {}\n".format(second, first)
+        elif second not in ftree.keys() and first in ftree.keys() and first not in [j for j in [i for i in ftree.values()] if first not in j][0]:
+            return "{} is the Grandmother of {}".format(first, second)
+    except:
+        pass
+
+
+def case6():
+    try:
+        if first == second:
+            return "Both typed name is {}, A same Person/Name.".format(first)
+    except:
+        pass
+
+
 def mem_search():
     global first
-    first = raw_input("Type the name of first member: ")
+    first = raw_input("Type the name of first member: ").capitalize()
     print("")
-    if first in ftree.keys() or True in [first in ftree.values()[i] for i in range(len(ftree.values()))]:
-        global second
-        try:
-            second = raw_input("Type the name of second member: ")
-            print("")
-            if second not in ftree.keys() and True not in [second in ftree.values()[i] for i in range(len(ftree.values()))] and False in [second in ftree.values()[i] for i in range(len(ftree.values()))] :
-                sures = raw_input("{} is not found, do you want to add it now. 'y' or 'n': ".format(second.upper()))
-                while True:
-                    if re.match('^n$',sures,re.I):
-                        systm_exit()
-                    elif re.match('^y$',sures,re.I):
-                        mem_add()
-                    else:
-                        print("Error: input not valid: Try again.\n")
-            elif second in ftree.keys() and first in ftree.get(second):
-                print("{} is mother of {}".format(second.upper(),first.upper()))
-            elif first in ftree.keys() and second in ftree.get(first):
-                print("{} is the mother of {}".format(first.upper(),second.upper()))
-            elif  [first in ftree.values()[i] for i in range(len(ftree.values()))].index(True) == [second in ftree.values()[i] for i in range(len(ftree.values()))].index(True):
-                mother = ftree.items()[[first in ftree.values()[i] for i in range(len(ftree.values()))].index(True)][0]
-                print("{} and {} are sisters and their mother is {}".format(first.upper(),second.upper(),mother.upper()))
-            else:
-                print("No direct relation found, Try Again...\n")
-        except ValueError:
-            print("{} has not mother listed here, It means {} is the oldest lady".format(first.upper(),first.upper()))
-    else:
-        mem_add()
 
+    if first in ftree.keys() or True in [first in ftree.values()[i] for i in range(len(ftree.values()))]:
+            global second
+            second = raw_input("Type the name of second member: ").capitalize()
+
+            if second not in ftree.keys() and True not in [second in ftree.values()[i] for i in range(len(ftree.values()))]:
+                mem_add_sure()
+            else:
+
+                if case6() is not None:
+                    print(case6())
+
+                elif case1() is not None:
+                    print(case1())
+
+                elif case2() is not None:
+                    print(case2())
+
+                elif case4() is not None:
+                    print(case4())
+
+                elif case5() is not None:
+                    print(case5())
+
+                elif case3() is not None:
+                    print(case3())
+    else:
+        mem_add_sure()
+
+
+# Main Programme which calls() all above functions as per requirement.
 if __name__ == '__main__':
   system('clear')
   while True:
     sure = raw_input("Do you want to continue 'Yes' or 'No': ")
-    if re.match('^no$|^n$',sure,re.I):
+    if re.match('^no$|^n$', sure, re.I):
         systm_exit()
 
-    elif re.match('^yes$|^y$',sure,re.I):
+    elif re.match('^yes$|^y$', sure, re.I):
         print("You selected to continue with programme.\n")
 
         while True:
             userinput = raw_input("press '1' to display the family tree, '2' to display the relationship of two members or '3' to exit.: ")
-
             if userinput == '1':
                 print("FAMILY TREE\n")
                 mem_tree()
@@ -116,9 +240,9 @@ if __name__ == '__main__':
                 if not bool(ftree):
                     while True:
                         suren = raw_input("FAMILY TREE IS EMPTY,\t Do you want to create one. type 'Yes' to continue or 'No' to exit. ")
-                        if re.match('^no$|^n$',suren,re.I):
+                        if re.match('^no$|^n$', suren, re.I):
                             systm_exit()
-                        elif re.match('^yes$|^y$',suren,re.I):
+                        elif re.match('^yes$|^y$', suren, re.I):
                             print("You selected to add the member.\n")
                             mem_search()
                             break
@@ -134,8 +258,3 @@ if __name__ == '__main__':
 
     else:
         print("Provide Input either 'Yes|Y' or 'No|N' ")
-
-#while True:
-
-
-
